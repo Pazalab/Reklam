d187a86d5eb26aee655bb4984da18593de40435e
justallyt <albertjunk2030@gmail.com>
2024-07-21 17:42:16 +0300

    Updates

--- src/components/home/AboutSection.jsx
+++ src/components/home/AboutSection.jsx
@@ -5,7 +5,37 @@ const AboutSection = () => {
     <div className="about-section">
               <div className="inner-row">
                         <div className="about-section-content">
-                                  
+                                   <div className="about-intro">
+                                             <h3>About Reklam</h3>
+                                             <h1>Elevate <span>Your Brand</span>. Amplify <span>Your Impact</span>. We&apos;re your Premier Marketing Agency.</h1>
+                                   </div>
+                                   <div className="about-description">
+                                              <div className="about-column-empty"></div>
+                                              <div className="about-description-texts">
+                                                           <h3>We use <span>Design</span>, <span>Technology</span>, and <span>marketing</span> to shape Digital Brand Experiences that propel your success in the digital realm.</h3>
+                                                           <p>At Reklam Solutions, we&apos;e your trusted partner in digital transformation, empowering every step of your journey. Whether you&apos;re a budding startup, a seasoned corporation seeking optimization, a government entity driving change, or an NGO amplifying impact, we walk alongside you. </p>
+                                                           <p>Our passionate experts leverage cutting-edge technology to fuel your unique journey. We believe in transparency and relentless dedication – your success is our greatest reward.</p>
+                                              </div>
+                                   </div>
+
+                                   <div className="numbers-row">
+                                              <div className="number-moja">
+                                                       <h1>50<sup>+</sup></h1>
+                                                       <p>Projects Completed</p>
+                                              </div>
+                                              <div className="number-moja">
+                                                         <h1>5<sup>+</sup></h1>
+                                                         <p>Years in Business</p>
+                                              </div>
+                                              <div className="number-moja">
+                                                          <h1>8</h1>
+                                                          <p>Team Members</p>
+                                              </div>
+                                              <div className="number-moja">
+                                                        <h1>10k</h1>
+                                                        <p>Lives impacted</p>
+                                              </div>
+                                   </div>
                         </div>
               </div>
     </div>
